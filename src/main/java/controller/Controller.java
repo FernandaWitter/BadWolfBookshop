fbe06688ce493429dda811564af9462614b11669
Fernanda <fernanda.witter@yahoo.com>
2020-03-10 09:12:42 -0300

    fix: corrects implementation

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -19,44 +19,44 @@ public class Controller extends HttpServlet {
 
     public Controller(){
         viewHelpers = new HashMap<>();
-//        viewHelpers.put("/badWolfBookshop/productList.jsp", new AdminBookVH());
+        viewHelpers.put("/badWolfBookshop/productList.jsp", new AdminBookVH());
 //        viewHelpers.put("/badWolfBookshop/bookRegistration.jsp", new AdminBookVH());
-
+//
         commands = new HashMap<>();
-        commands.put("create", new CreateCmd());
-        commands.put("update", new UpdateCmd());
-        commands.put("delete", new DeleteCmd());
-        commands.put("find", new FindCmd());
+//        commands.put("create", new CreateCmd());
+//        commands.put("update", new UpdateCmd());
+//        commands.put("delete", new DeleteCmd());
+//        commands.put("find", new FindCmd());
     }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response){
         try{
-            String uri = request.getRequestURI();
-
-            IViewHelper vh = viewHelpers.get(uri);
-            DomainObject object = vh.getObject(request);
-
-            String operation;
-
-            if(null == request.getAttribute("operation"))
-                operation = request.getParameter("operation");
-            else
-                operation = request.getAttribute("operation").toString();
-            if(operation == null){
-                if(uri.contains("/admin/"))
-                    operation = "find";
-                else
-                    operation = "find";
-            }
-
-            ICommand cmd;
-            Result result;
-
-            cmd = commands.get(operation);
-            result = cmd.execute(object);
-
-            vh.setView(request, response, result);
+//            String uri = request.getRequestURI();
+//
+//            IViewHelper vh = viewHelpers.get(uri);
+//            DomainObject object = vh.getObject(request);
+//
+//            String operation;
+//
+//            if(null == request.getAttribute("operation"))
+//                operation = request.getParameter("operation");
+//            else
+//                operation = request.getAttribute("operation").toString();
+//            if(operation == null){
+//                if(uri.contains("/admin/"))
+//                    operation = "find";
+//                else
+//                    operation = "find";
+//            }
+//
+//            ICommand cmd;
+//            Result result;
+//
+//            cmd = commands.get(operation);
+//            result = cmd.execute(object);
+//
+//            vh.setView(request, response, result);
 
         } catch (Exception e){
             e.printStackTrace();

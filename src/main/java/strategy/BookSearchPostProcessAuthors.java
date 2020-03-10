6b0f0975aafba1eaa084f0e80d5fc250060f2774
Fernanda <fernanda.witter@yahoo.com>
2020-03-10 09:09:22 -0300

    refactor: corrects implementation

--- src/main/java/strategy/BookSearchPostProcessAuthors.java
+++ src/main/java/strategy/BookSearchPostProcessAuthors.java
@@ -5,6 +5,7 @@ import domain.Book;
 import domain.DomainObject;
 import domain.Result;
 import repository.AuthorDAO;
+import repository.BookDAO;
 
 import java.util.ArrayList;
 
@@ -12,12 +13,16 @@ public class BookSearchPostProcessAuthors implements IStrategy {
     @Override
     public void process(DomainObject object, Result result) {
         AuthorDAO authorDAO = new AuthorDAO();
+        BookDAO bookDAO = new BookDAO();
         Book book = (Book) object;
-        authorDAO.findById(book, result);
+        bookDAO.findAuthors(book, result);
         ArrayList<Author> authors = new ArrayList<>();
-        for(DomainObject d : result.getObject(Author.class.getSimpleName())){
-            Author a = (Author)d;
+        for(DomainObject d : result.getObject(Author.class.getSimpleName())) {
+            authorDAO.findById(d, result);
+            Author a = (Author) result.getObject(Author.class.getSimpleName()).get(0);
+            result.removeObject(Author.class.getSimpleName());
             authors.add(a);
         }
+        book.setAuthors(authors);
     }
 }

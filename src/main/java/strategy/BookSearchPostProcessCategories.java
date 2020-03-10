4d908261174d6b8d3d2c199d640cc08472ea154c
Fernanda <fernanda.witter@yahoo.com>
2020-03-10 09:09:12 -0300

    refactor: corrects implementation

--- src/main/java/strategy/BookSearchPostProcessCategories.java
+++ src/main/java/strategy/BookSearchPostProcessCategories.java
@@ -2,6 +2,7 @@ package strategy;
 
 import domain.*;
 import repository.AuthorDAO;
+import repository.BookDAO;
 import repository.CategoryDAO;
 
 import java.util.ArrayList;
@@ -10,12 +11,16 @@ public class BookSearchPostProcessCategories implements IStrategy {
     @Override
     public void process(DomainObject object, Result result) {
         CategoryDAO categoryDAO = new CategoryDAO();
+        BookDAO bookDAO = new BookDAO();
         Book book = (Book) object;
-        categoryDAO.findById(book, result);
+        bookDAO.findCategories(book, result);
         ArrayList<Category> categories = new ArrayList<>();
-        for(DomainObject d : result.getObject(Author.class.getSimpleName())){
-            Category c = (Category)d;
+        for(DomainObject d : result.getObject(Category.class.getSimpleName())) {
+            categoryDAO.findById(d, result);
+            Category c = (Category) result.getObject(Category.class.getSimpleName()).get(0);
+            result.removeObject(Category.class.getSimpleName());
             categories.add(c);
         }
+        book.setCategories(categories);
     }
 }

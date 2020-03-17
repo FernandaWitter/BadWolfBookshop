fc7314dbe5132900e5938633e219dd012db2286c
Fernanda <fernanda.witter@yahoo.com>
2020-03-17 19:38:40 -0300

    feat: implements ISBN-13 validation

--- src/main/java/strategy/IsbnValidation.java
+++ src/main/java/strategy/IsbnValidation.java
@@ -8,8 +8,13 @@ public class IsbnValidation implements IStrategy {
     @Override
     public void process(DomainObject object, Result result) {
         Book book = (Book) object;
-        if(!isValidISBN(book.getIsbn())){
-            result.setMsg("error", "O ISBN informado n&atilde;o &eacute; v&acute;lido!");
+        boolean isValid;
+        if(book.getIsbn().length() == 10)
+            isValid = isValidISBN(book.getIsbn());
+        else
+            isValid = validateIsbn13(book.getIsbn());
+        if(!isValid){
+            result.setMsg("error", "O ISBN informado n&atilde;o &eacute; v&aacute;lido!");
         }
     }
 
@@ -44,5 +49,46 @@ public class IsbnValidation implements IStrategy {
         // Return true if weighted sum
         // of digits is divisible by 11.
         return (sum % 11 == 0);
+    }
+
+    public boolean validateIsbn13( String isbn )
+    {
+        if ( isbn == null )
+        {
+            return false;
+        }
+
+        //remove any hyphens
+        isbn = isbn.replaceAll( "-", "" );
+
+        //must be a 13 digit ISBN
+        if ( isbn.length() != 13 )
+        {
+            return false;
+        }
+
+        try
+        {
+            int tot = 0;
+            for ( int i = 0; i < 12; i++ )
+            {
+                int digit = Integer.parseInt( isbn.substring( i, i + 1 ) );
+                tot += (i % 2 == 0) ? digit * 1 : digit * 3;
+            }
+
+            //checksum must be 0-9. If calculated as 10 then = 0
+            int checksum = 10 - (tot % 10);
+            if ( checksum == 10 )
+            {
+                checksum = 0;
+            }
+
+            return checksum == Integer.parseInt( isbn.substring( 12 ) );
+        }
+        catch ( NumberFormatException nfe )
+        {
+            //to catch invalid ISBNs that have non-numeric characters in them
+            return false;
+        }
     }
 }
